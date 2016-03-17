745a879c64eea95ced7d6fc7799a8643c8801bad
markmer <mark.merezhnikov@mail.ru>
2016-03-17 21:25:03 +0300

    bcd

---
+++ Module_2/Chapter_1/Task1_2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{int day,month;
+	day=26;
+	month=06;
+printf("%d/%d",day,month);
+return 0;}
---
+++ Module_2/Chapter_1/Task1_3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{int day,month,comb;
+	day=26;
+	month=06;
+	comb=day;
+	day=month;
+	month=comb;
+	printf("%d/%d",day,month);
+return 0;}
