5a1e1651e3a510e9e88554f52dc81931fcfa9619
markmer <mark.merezhnikov@mail.ru>
2016-04-26 18:01:30 +0300

    correct

--- Module_2/Chapter_3/Task2_3_6.c
+++ Module_2/Chapter_3/Task2_3_6.c
@@ -3,14 +3,18 @@
 int main()
 {
 	int a,i;
-	printf("Введите процент прогресса\n");
+re:	printf("Введите процент прогресса\n");
 	scanf("%d",&a);
 	if(a>100)
-		{printf("КПД больше 100%% это миф\n"); return 0;}
+		{printf("КПД больше 100%% это миф\n"); goto re;}
+	if(a<0)
+		{printf("Прогресс должен быть положительным\n");goto re;}
 	putchar('[');
-	for(i=0;i<=a;i=i+5)
-		putchar('=');
-	for (;i<=100;i=i+5)
+	if(a==0)
+		goto f2;
+	for(i=0;i<=a;i+=5)
+			putchar('=');
+f2:	for (;i<=100;i=i+5)
 		putchar(' ');
 	putchar(']');
 	printf("\n");
